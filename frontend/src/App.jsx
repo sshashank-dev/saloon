e614ae12e1ccd9ed2b709737dc08d7efecd42e39
shash <shashanksharma55204@gmail.com>
2026-08-11 17:49:57 +0530

    Improve mobile responsive layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import "./App.css";
 
 import Clock from "./components/Clock";
@@ -28,9 +27,36 @@ function App() {
       {/* Music player */}
       <MusicPlayer />
 
+      {/* Contact */}
+      <div
+        className="contact-link"
+        style={{
+          marginTop: "670px",
+          fontSize: "10px",
+          fontFamily: "'Poppins', sans-serif",
+          fontWeight: "bold",
+          color: "white",
+          textAlign: "center",
+          opacity: 0.8,
+        }}
+      >
+        Contact @{" "}
+        <a
+          href="https://www.linkedin.com/in/shashank-sharma-9b7b2b257/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            fontWeight: "600",
+          }}
+        >
+          Shashank Sharma
+        </a>
+      </div>
+
     </main>
   );
 }
 
 export default App;
-
